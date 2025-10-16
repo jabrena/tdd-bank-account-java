2e7b2adfae1388663cd46ad8731de9e6acf463d3
Juan Antonio Breña Moral <bren@juanantonio.info>
2025-10-16 19:44:04 +0200

    Fixing test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,16 @@
 package org.xpdojo.bank;
 
+import java.util.function.IntPredicate;
+
 public class Account {
+
+    public void deposit(int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+    }
+
+    public IntPredicate getBalance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBalance'");
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,10 +4,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ *  1. I can **Deposit** money to accounts
+ *  1. I can **Withdraw** money from accounts
+ *  1. I can **Transfer** amounts between accounts (if I have the funds)
+ *  1. I can print out an Account balance slip (date, time, balance)
+ *  1. I can print a statement of account activity (statement)
+ *  1. I can apply Statement filters (include just deposits, withdrawal, date)
+ */
 public class AccountTest {
 
     @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat(true).isFalse();
+
+        // Given
+        int amount = 100;
+        Account account = new Account();
+
+        // When
+        account.deposit(amount);
+
+        // Then
+        assertThat(account.getBalance()).isEqualTo(amount); // 100
     }
 }
