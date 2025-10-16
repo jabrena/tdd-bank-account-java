57cdee7b8fb390aea458a8961c2e9968d105b353
Juan Antonio Breña Moral <bren@juanantonio.info>
2025-10-16 19:40:04 +0200

    Setup project

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,16 +1,13 @@
 package org.xpdojo.bank;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Ignore
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test isn't implemented", true, is(false));
+        assertThat(true).isFalse();
     }
 }
